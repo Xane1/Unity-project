a262a441399cf561ce1bd828636c233fb6038771
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2023-12-17 16:42:27 +0000

    To revert, destroyed my stuff

--- Assets/Scripts/Ejay/Bullet.cs
+++ Assets/Scripts/Ejay/Bullet.cs
@@ -3,15 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class BulletBounce : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private Rigidbody2D rb;
+    private Vector3 lastVelocity;
+
+    private void Awake()
     {
-        Destroy(gameObject);
-        //Enemy, wall hit
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log(coll.transform.tag);
+        if (coll.transform.tag == "Deflect")
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+
+            rb.velocity = direction * MathF.Max(speed, 0f);
+        }
+        else
+        {
+            Destroy(gameObject);
+            //Enemy, wall hit
+        }
     }
     void OnBecameInvisible() {
         Destroy(gameObject);
     }
-    
 }
--- Assets/Scripts/PlayerMovementImproved.cs
+++ Assets/Scripts/PlayerMovementImproved.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovementImproved : MonoBehaviour
 {
-    public float moveSpeed = 0;
+    public float speed = 8f;
+    private bool isFacingRight = true;
     public Rigidbody2D rb;
     public Weapon weapon;
     private float horizontal;
 
-    private Vector2 moveDirection;
     private Vector2 mousePosition;
+    private Vector2 moveDirection;
+    
+
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-
+        
+        Flip();
+        
         if (Input.GetMouseButtonDown(0))
         {
             weapon.Fire();
@@ -25,10 +30,21 @@ public class PlayerController : MonoBehaviour
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y);
-
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        
         Vector2 aimdirection = mousePosition - rb.position;
         float aimAngle = MathF.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = aimAngle;
