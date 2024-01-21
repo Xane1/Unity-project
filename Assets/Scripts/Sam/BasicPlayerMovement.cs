707e4f3396812e7a92588fd9aab5b2f8bdd5eefb
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2024-01-21 15:48:30 +0000

    File changes.

--- Assets/Scripts/Sam/BasicPlayerMovement.cs
+++ Assets/Scripts/Sam/BasicPlayerMovement.cs
@@ -8,6 +8,8 @@ public class BasicPlayerMovement : MonoBehaviour
     [SerializeField] private float characterSpeed = 5f;
     
     private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] bool playerCanMove;
     
     // Start is called before the first frame update
     void Start()
@@ -25,7 +27,7 @@ public class BasicPlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody2D.velocity = new Vector2(_movementDirection * characterSpeed, _rigidbody2D.velocity.y);
+        if (playerCanMove) _rigidbody2D.velocity = new Vector2(_movementDirection * characterSpeed, _rigidbody2D.velocity.y);
     }
 }
 
